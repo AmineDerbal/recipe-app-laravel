4097f0684b2d89031f27fc17306ac6db2bd124f8
AmineDerbal <aminedoran31@gmail.com>
2024-03-12 13:47:47 +0100

    update home view page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{asset('css/home.css')}}" />
-</head>
-<body>
-   <div class="main_container">
+
+
+    @extends('layouts.app')
+
+
+@section('content') 
+<div class="main_container">
     <div class="home_container">
         <div class="heading">
 <h1 class="home_title">Welcome to Our Recipe App!</h1>
@@ -20,6 +17,5 @@
     </div>
 <img src="{{asset('images/salad.png')}}" class="salad" alt="salad"  />
    </div>
-    
-</body>
-</html>
+
+@endsection
