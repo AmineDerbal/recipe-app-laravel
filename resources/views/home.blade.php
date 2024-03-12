9cf7999f66eaf234def02329e3e522261a4873cc
AmineDerbal <aminedoran31@gmail.com>
2024-03-12 12:32:13 +0100

    Create home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/home.css')}}" />
+</head>
+<body>
+   <div class="main_container">
+    <div class="home_container">
+        <div class="heading">
+<h1 class="home_title">Welcome to Our Recipe App!</h1>
+<h1 class="home_title_h2">Find recipes for delicious meals</h1>
+<a href="" class="btn_home">See recipes</a>
+
+        </div>
+        <img src="{{asset('images/bowl.png')}}" class="bowl" alt="bowl"  />
+
+    </div>
+<img src="{{asset('images/salad.png')}}" class="salad" alt="salad"  />
+   </div>
+    
+</body>
+</html>
