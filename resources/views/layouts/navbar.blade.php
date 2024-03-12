bc98e9aa658a8396cc0d406b6f42a9e0418f607e
AmineDerbal <aminedoran31@gmail.com>
2024-03-12 13:46:26 +0100

    Create the navbar page

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,12 @@
+
+    <nav>
+        <ul class="nav_list">
+        
+            <li class="sign_out">
+            <p class="nav_name">Hey </p>
+     
+            </li>
+       
+        </ul>
+    </nav>
+
