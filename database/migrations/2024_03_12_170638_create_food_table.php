823ed8e150fcb768a9d8bf4857a88b21a738c7d1
Amine derbal <aminedoran31@gmail.com>
2024-03-12 18:57:32 +0100

    Update food table migration

--- database/migrations/2024_03_12_170638_create_food_table.php
+++ database/migrations/2024_03_12_170638_create_food_table.php
@@ -12,7 +12,7 @@ return new class () extends Migration {
      */
     public function up()
     {
-        Schema::create('foods', function (Blueprint $table) {
+        Schema::create('food', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('measurement_unit');
