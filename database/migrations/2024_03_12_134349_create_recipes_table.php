0ee82561894d6373e08eea3bb3b068721129d68c
AmineDerbal <aminedoran31@gmail.com>
2024-03-12 15:13:01 +0100

    Create recipes migration

---
+++ database/migrations/2024_03_12_134349_create_recipes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('preparation_time');
+            $table->decimal('cooking_time');
+            $table->text('description');
+            $table->boolean('is_public')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
