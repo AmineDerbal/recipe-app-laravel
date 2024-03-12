4989795730b250e0c8bb65dde5af0ef52d817547
AmineDerbal <aminedoran31@gmail.com>
2024-03-12 14:14:01 +0100

    update navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,12 +1,21 @@
 
     <nav>
         <ul class="nav_list">
-        
+            <li><a href="{{ route('home') }}" class="nav-item mobile-home"> Home</a></li>
+            @if(auth()->check())
             <li class="sign_out">
-            <p class="nav_name">Hey </p>
-     
+            <p class="nav_name">Hey {{ auth()->user()->name }}</p>
+            <p>logout</p>     
             </li>
+            @else
+            <li >Sign in</li>
+            @endif
        
         </ul>
     </nav>
-
+    
+    <!-- @if(auth()->check()) 
+    <h1>Welcome {{ auth()->user()->name }}</h1>
+    @else 
+    <h1>Welcome Guest</h1>
+    @endif -->
