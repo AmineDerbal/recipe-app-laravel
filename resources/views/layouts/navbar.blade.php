c3eeb5b833af5f5a2afa7826a074501f9748bc43
AmineDerbal <aminedoran31@gmail.com>
2024-03-14 15:26:10 +0100

    Update navbar to show current user

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,16 +1,26 @@
     <nav>
         <ul class="nav_list">
-            <li><a href="{{ route('home') }}" class="nav-item mobile-home"> Home</a></li>
-            @if(auth()->check())
-            <li class="sign_out">
-            <p class="nav_name">Hey {{ auth()->user()->name }}</p>
-            <p>logout</p>     
-            </li>
-            @else
-            <li >Sign in</li>
-            @endif
-       
+            <li class="nav-item mobile-home"><a class="nav-item mobile-home" href="{{ route('home') }}" > Home</a></li>
+            <li ><a class="nav-item" href="{{route('food')}}">Foods</a></li>
+@auth 
+{{-- User is logged in --}}
+<li class="sign_out">
+<p class="nav_name">Hey {{ Auth::user()->name }}</p>
+<form action="{{ route('logout') }}" method="POST">
+    @csrf
+    <button class="nav-btn" type="submit">Sign out</button>
+</form>
+@else
+{{-- User is not logged in --}}
+<li>
+    <form action="{{ route('login.show') }}" method="GET">
+    <button class="nav-btn" type="submit">Sign in</button>
+</form>
+
+</li>
+@endauth
         </ul>
     </nav>
     
 
+ 
