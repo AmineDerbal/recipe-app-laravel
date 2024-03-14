ae9fa1d497860ca8bb7eef1af06b9a89110ca98d
AmineDerbal <aminedoran31@gmail.com>
2024-03-14 15:27:12 +0100

    Create new page to add food

---
+++ resources/views/food/new.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+
+@if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form method="POST" action="{{ route('food.create') }}">
+        @csrf
+       <div class="field">
+       <label class="label" style="display: block;">Name</label>
+       <input class="input" type="text" name="name" required>
+</div>
+<div class="field">
+       <label class="label" style="display: block;">Measurement_unit</label>
+       <input class="input" type="text" name="measurement_unit" required>
+</div>
+<div class="field">
+    <label class="label" style="display: block;">Quantity</label>
+    <input class="input" type="number" name="quantity" required>
+</div>
+<div class="field">
+       <label class="label" style="display: block;">Price</label>
+       <input class="input" type="number" name="price" required>
+</div>
+
+<button type="submit" class="btn-submit">Create food</button>
+
+    </form>
+
+
+@endsection
