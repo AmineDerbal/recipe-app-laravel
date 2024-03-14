48b569f042f88d61b42ac261aced59415fed9d4d
AmineDerbal <aminedoran31@gmail.com>
2024-03-14 15:27:21 +0100

    Update routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Auth\LoginRegisterController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\FoodController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,10 +18,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[HomeController::class, 'index'])->name('home');
-Route::controller(LoginRegisterController::class)->group(function () {
-    Route::get('/register', 'register')->name('register');
-    Route::get('/login', 'login')->name('login');
-    Route::post('/logout', 'logout')->name('logout');
+ Route::get('/',[HomeController::class, 'index'])->name('home')->middleware('auth');
+ Route::controller(FoodController::class)->group(function () {
+     Route::get('/food', 'index')->name('food');
+     Route::get('/food/new', 'new')->name('food.new');
+     Route::post('/food', 'create')->name('food.create');
+     Route::delete('/food/{food}', 'destroy')->name('food.destroy');
+ });
+
+
+Route::controller(RegisterController::class)->group(function () {
+    Route::get('/register', 'show')->name('register.show');
+    Route::post('/register', 'store')->name('register.store');
+});
+
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'show')->name('login.show');
+    Route::post('/login', 'login')->name('login');
+    Route::post('/logout', 'destroy')->name('logout');
 });
 
