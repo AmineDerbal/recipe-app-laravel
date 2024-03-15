1f26451ee117542d93c060222f855361784956e5
Amine derbal <aminedoran31@gmail.com>
2024-03-15 18:38:51 +0100

    Fix bug in display

--- resources/views/recipe/index.blade.php
+++ resources/views/recipe/index.blade.php
@@ -4,13 +4,13 @@
 <h1 class='header-title'>List of All recipes of {{ $user->name }}</h1>
 <h2> <a href="{{ route('recipe.new') }}">Add Recipe</a></h2>
 
-@if($recipes->empty())
+@if($recipes->isEmpty())
 <h3 class='header-title'>No recipes found</h3>
 @else
 @foreach ($recipes as $recipe)
 <div class='container-md recipe-container border border-dark'>
   <div class='recipe_name'>
-    <h3><a href="/recipe/{{ $recipe->id }}">{{ $recipe->name }}</a></h3>
+    <h3><a href="{{route('recipe.show',$recipe->id)}}">{{ $recipe->name }}</a></h3>
     @if($user->id == $recipe->user_id)
     <form method="POST" action="{{route('recipe.destroy', $recipe->id)}}">
       @csrf
