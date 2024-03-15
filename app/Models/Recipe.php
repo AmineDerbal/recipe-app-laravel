5ac0001d13bd4e3e96d334f116618ff31fed4d46
Amine derbal <aminedoran31@gmail.com>
2024-03-15 12:39:06 +0100

    Update Recipe mode with protected variables

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'description', 'preparation_time', 'cooking_time','is_public','user_id'];
 }
