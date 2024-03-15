611377bdb653e18dfd8aafd378cd05aa944c1e59
Amine derbal <aminedoran31@gmail.com>
2024-03-15 18:37:19 +0100

    Add tables relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,9 @@ use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function recipes()
+    {
+        return $this->hasMany(Recipe::class, );
+    }
+
+    public function food()
+    {
+        return $this->hasMany(Food::class);
+    }
 }
