7c758f488204557ad7dd484a41b6da802c8e49f3
Amine derbal <aminedoran31@gmail.com>
2024-03-15 18:36:43 +0100

    Add table relationship

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -10,4 +10,11 @@ class Food extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'price', 'measurement_unit', 'quantity', 'user_id'];
+
+    public function recipes()
+    {
+        return $this->belongsToMany(Recipe::class, 'recipe_food')->withPivot('quantity');
+    }
+
+
 }
--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -9,4 +9,10 @@ class Recipe extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'description', 'preparation_time', 'cooking_time','is_public','user_id'];
+
+    public function food()
+    {
+        return $this->belongsToMany(Food::class, 'recipe_food')->withPivot('quantity');
+    }
+
 }
