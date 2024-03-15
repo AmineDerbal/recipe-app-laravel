27297ae61c731b5f59eff2b71af6c3361b4972e3
Amine derbal <aminedoran31@gmail.com>
2024-03-15 11:31:10 +0100

    Update register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,28 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>Register</h2>
-<form method="post" action="{{ route('register.store') }}">
-@csrf
+<div class="container-login">
+  <h2 class="title_login">Sign up</h2>
 
-<div class="mb-3">
-    <label for="name" class="form-label">Name</label>
-    <input type="text" class="form-control" id="name" name="name">
-</div>
-<div class="mb-3">
-    <label for="email" class="form-label">Email</label>
-    <input type="email" class="form-control" id="email" name="email">
-</div>
-<div class="mb-3">
-    <label for="password" class="form-label">Password</label>
-    <input type="password" class="form-control" id="password" name="password">
-</div>
-<div class="mb-3"> 
-    <label for="password-confirm" class="form-label">Confirm Password</label>
-    <input type="password" class="form-control" id="password-confirm" name="c_password">
-</div>
-<button type="submit" class="btn btn-primary">Register</button>
+  <form method="POST" action="{{ route('register.store') }}">
+    @csrf
+    <div class="field">
+      <label for="name" class="label">Name</label>
+      <br>
+      <input id="name" type="text" class="input" name="name" required autocomplete="name" autofocus>
+    </div>
+    <div class="field">
+      <label for="email" class="label">Email</label>
+      <br>
+      <input id="email" type="email" class="input" name="email" required autocomplete="email" autofocus>
+    </div>
+    <div class="field">
+      <label for="password" class="label">Password</label>
+      <br>
+      <input id="password" type="password" class="input" name="password" required autocomplete="new-password">
+    </div>
+    <div class="field">
+      <label for="password_confirmation" class="label">Password confirmation</label>
+      <br>
+      <input id="password_confirmation" type="password" class="input" name="c_password" required
+        autocomplete="new-password">
+    </div>
+    <div class="actions"> <input type="submit" name='commit' class="btn-submit" value="Sign up"> </div>
+  </form>
+  @include('shared.links')
 
-</form>
+</div>
 
 @endsection
