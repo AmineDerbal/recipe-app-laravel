098666c2d67bf6d0d028829b641558c0eaf35b60
Amine derbal <aminedoran31@gmail.com>
2024-03-15 18:38:23 +0100

    Format file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,20 @@
+@extends('layouts.app')
 
 
-    @extends('layouts.app')
-
-
-@section('content') 
+@section('content')
 
 <div class="main_container">
-    <div class="home_container">
-        <div class="heading">
-<h1 class="home_title">Welcome to Our Recipe App!</h1>
-<h1 class="home_title_h2">Find recipes for delicious meals</h1>
-<a href="" class="btn_home">See recipes</a>
-
-        </div>
-        <img src="{{asset('images/bowl.png')}}" class="bowl" alt="bowl"  />
+  <div class="home_container">
+    <div class="heading">
+      <h1 class="home_title">Welcome to Our Recipe App!</h1>
+      <h1 class="home_title_h2">Find recipes for delicious meals</h1>
+      <a href="" class="btn_home">See recipes</a>
 
     </div>
-<img src="{{asset('images/salad.png')}}" class="salad" alt="salad"  />
-   </div>
+    <img src="{{asset('images/bowl.png')}}" class="bowl" alt="bowl" />
+
+  </div>
+  <img src="{{asset('images/salad.png')}}" class="salad" alt="salad" />
+</div>
 
 @endsection
