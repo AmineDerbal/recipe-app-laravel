e7f26e34f8bf405c0f53b3d970f043975a9d48b4
Amine derbal <aminedoran31@gmail.com>
2024-03-16 17:06:41 +0100

    Handle login errors

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -20,12 +19,18 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
+
+
         $credentials = $request->only('email', 'password');
         $remember = $request->filled('remember'); //remember me
 
         if (Auth::attempt($credentials, $remember)) {
             $request->session()->regenerate();
             return redirect()->intended('/');
+        } else {
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ]);
         }
 
 
