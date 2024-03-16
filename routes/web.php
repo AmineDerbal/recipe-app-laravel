e042c29bd8131d4f48d5e0648501dc9c7ecbe57e
Amine derbal <aminedoran31@gmail.com>
2024-03-16 17:05:01 +0100

    Update routing middleware

--- routes/web.php
+++ routes/web.php
@@ -21,34 +21,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home')->middleware('auth');
-Route::controller(FoodController::class)->group(function () {
-    Route::get('/food', 'index')->name('food');
-    Route::get('/food/new', 'new')->name('food.new');
-    Route::post('/food', 'create')->name('food.create');
-    Route::get('/food/{food}', 'show')->name('food.show');
-    Route::get('/food/{food}/edit', 'edit')->name('food.edit');
-    Route::put('/food/{food}', 'update')->name('food.update');
-    Route::delete('/food/{food}', 'destroy')->name('food.destroy');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => 'auth'], function () {
+    Route::controller(FoodController::class)->group(function () {
+        Route::get('/food', 'index')->name('food');
+        Route::get('/food/new', 'new')->name('food.new');
+        Route::post('/food', 'create')->name('food.create');
+        Route::get('/food/{food}', 'show')->name('food.show');
+        Route::get('/food/{food}/edit', 'edit')->name('food.edit');
+        Route::put('/food/{food}', 'update')->name('food.update');
+        Route::delete('/food/{food}', 'destroy')->name('food.destroy');
+    });
+
+    Route::controller(RecipeController::class)->group(function () {
+        Route::get('/recipe', 'index')->name('recipe.index');
+        Route::get('/recipe/new', 'new')->name('recipe.new');
+        Route::post('/recipe', 'create')->name('recipe.create');
+        Route::get('/recipe/{recipe}', 'show')->name('recipe.show');
+        Route::put('/recipe/{recipe}', 'toggle_public')->name('recipe.toggle_public');
+        Route::delete('/recipe/{recipe}', 'destroy')->name('recipe.destroy');
+    });
+
+    Route::controller(RecipeFoodController::class)->group(function () {
+        Route::get('/recipe/{recipe}/ingredient/new', 'new')->name('recipe_food.new');
+        Route::post('/recipe/{recipe}/ingredient', 'create')->name('recipe_food.create');
+        Route::delete('/recipe/ingredient/{recipeFood}', 'destroy')->name('recipe_food.destroy');
+    });
+
+    Route::get('/public_recipes', [PublicRecipesController::class, 'index'])->name('public_recipes');
+    Route::get('general_shopping_list', [GenrealShoppingListController::class, 'index'])->name('general_shopping_list');
+
 });
 
-Route::controller(RecipeController::class)->group(function () {
-    Route::get('/recipe', 'index')->name('recipe.index');
-    Route::get('/recipe/new', 'new')->name('recipe.new');
-    Route::post('/recipe', 'create')->name('recipe.create');
-    Route::get('/recipe/{recipe}', 'show')->name('recipe.show');
-    Route::put('/recipe/{recipe}', 'toggle_public')->name('recipe.toggle_public');
-    Route::delete('/recipe/{recipe}', 'destroy')->name('recipe.destroy');
-});
 
-Route::controller(RecipeFoodController::class)->group(function () {
-    Route::get('/recipe/{recipe}/ingredient/new', 'new')->name('recipe_food.new');
-    Route::post('/recipe/{recipe}/ingredient', 'create')->name('recipe_food.create');
-    Route::delete('/recipe/ingredient/{recipeFood}', 'destroy')->name('recipe_food.destroy');
-});
 
-Route::get('/public_recipes', [PublicRecipesController::class, 'index'])->name('public_recipes');
-Route::get('general_shopping_list', [GenrealShoppingListController::class, 'index'])->name('general_shopping_list');
+
+
+
+
+
 
 
 Route::controller(RegisterController::class)->group(function () {
