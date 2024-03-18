36dff535605f91a0de3e62846a08253081f4b5e8
AmineDerbal <aminedoran31@gmail.com>
2024-03-18 11:45:32 +0100

    Add link to resend verification email

--- resources/views/shared/links.blade.php
+++ resources/views/shared/links.blade.php
@@ -5,3 +5,8 @@
 @if(isset($controllerName) && $controllerName !== 'RegisterController')
 <p><a href="{{ route('register.show') }}">Register</a></p>
 @endif
+
+@if ((isset($controllerName) && $controllerName === 'LoginController') && (auth()->check() && !auth()->user()->email_verified_at))
+<p> <a href="{{route('resend.verification.email')}}">resend verification email</a></p>
+
+@endif
